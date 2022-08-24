5a1f9062e899795017a3b0fcd06bdabd5a172720
Fatima Niyaz <fatima.niyazova@gmail.com>
2022-08-24 21:53:29 +0600

    added objectdeletemixin, delete tag, post functions

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views import View
 
 from .models import Post, Tag
@@ -39,7 +40,15 @@ class TagUpdate(ObjectUpdateMixin, View):
     model_form = TagForm
     template = 'blog/tag_update_form.html'
 
-
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
     
 def tags_list(request):
     tags = Tag.objects.all()
